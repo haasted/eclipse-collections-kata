e645e333ea7e239f30e4329b48c394c9d55d3c7c
Henrik Aasted Sørensen <has@bitcraft.dk>
2017-03-31 10:46:41 +0200

    Company exercise 3.

--- company-kata/src/main/java/org/eclipse/collections/companykata/Company.java
+++ company-kata/src/main/java/org/eclipse/collections/companykata/Company.java
@@ -49,13 +49,7 @@ public class Company
 
     public MutableList<Order> getOrders()
     {
-        Assert.fail("Refactor this code to use Eclipse Collections as part of Exercise 3");
-        MutableList<Order> orders = FastList.newList();
-        for (Customer customer : this.customers)
-        {
-            orders.addAll(customer.getOrders());
-        }
-        return orders;
+        return customers.flatCollect(Customer::getOrders);
     }
 
     public Customer getMostRecentCustomer()
